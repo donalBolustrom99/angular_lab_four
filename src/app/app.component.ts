20fddd4eabd5db4ed81d2e0695e3109901da98c2
Donal Bolustrom <donalbolustrom1999@gmail.com>
2021-03-19 11:45:26 +0000

    lab upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent 
+{
   title = 'data-binding-app';
+  numberOfTimes:number=0;
+  hidden=false;
+
+  count()
+  {
+    this.numberOfTimes++;
+  }
+
+  show()
+  {
+    if(this.hidden==true)
+    {
+      this.hidden = false;
+    }
+    else
+    {
+      this.hidden=true;
+    }
+  }
 }
